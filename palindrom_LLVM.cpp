558ee91b2ee53cb6ad039a5c072e086cb85da9d2
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-07-17 09:36:21 +0200

    rest of the taska

---
+++ palindrom_LLVM.cpp
@@ -0,0 +1,35 @@
+#include "Streams.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <cstring>
+#include <string>
+//------don't use library <iostream>-----//
+//using namespace std;
+
+std::string Sentence;
+
+bool Palindrom(std::string Sentence){
+	std::string InterSenc = "";
+       	int Lgth =Sentence.length();
+     for (int i = Lgth - 1; i >= 0; i--){
+       InterSenc += Sentence[i];
+     }
+     if (Sentence == InterSenc) {
+	 return true;
+     }
+     else
+     return false;
+}
+
+int main(){
+	llvm::cout<<"Podaj napis :"<<std::fflush;
+	llvm::cin>>Sentence;
+    if(Palindrom(Sentence)){
+	 llvm::cout<<"true"<<std::fflush;
+    } else
+	   llvm::cout<<"false"<<std::fflush;
+    
+    return 0;
+}
+
